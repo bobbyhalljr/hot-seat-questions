ad0b8e9b4555ff4f903b223ad5c5a792d8383e06
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-08-16 23:50:55 -0400

    updated hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import { Stack, Text } from '@chakra-ui/core'
 
 export const Hero = ({ title, subTitle }) => (
-  <Stack spacing={[8, 12]} maxWidth="52rem" mx={4} mt={['6rem', '10rem']} mb={['3rem', '4rem', '7rem']}>
+  <Stack spacing={[8, 12]} maxWidth="52rem" mx={4} mt={['6rem', '10rem']} mb={['6rem', '7rem', '8rem']}>
     <Text fontWeight='extrabold' letterSpacing={0.1} fontSize={["2.5rem", "3.5rem"]}>{title}</Text>
-    <Text letterSpacing={1.2} fontWeight='medium' fontSize={["1.5rem", "2rem"]}>{subTitle}</Text>
+    <Text fontWeight='medium' fontSize={["1.5rem", "2rem"]}>{subTitle}</Text>
   </Stack>
 )
 
